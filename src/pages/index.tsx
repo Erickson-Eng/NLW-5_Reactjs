5add7beba23e325318be8214bb89c0d5bb44ce92
erickson <erickson.tulio@academico.ifpb.edu.br>
2021-04-21 15:16:39 -0300

    aula 1, 2 e 3

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episodes[];
+}
+
+export default function Home(props: HomeProps) {
+
+  return (
+    <div>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+
+  })
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, //funciona a cada 8 horas
+  }
+}
+
